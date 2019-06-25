343016a1fd0f1903c3cb87df07dda4bbe6504176
Andrew42 <awightma@nd.edu>
2019-06-25 16:05:26 -0400

    Add additional examples to add datasets

--- Reader/python/update_datasets.py
+++ Reader/python/update_datasets.py
@@ -15,11 +15,8 @@ GIT_REPO_DIR = subprocess.check_output(['git','rev-parse','--show-toplevel']).st
 DATASET_FILE = os.path.join(GIT_REPO_DIR,args.dir,args.inf)
 SAVE_FILE    = os.path.join(GIT_REPO_DIR,args.dir,args.outf)
 
-# Various examples of how to add/update/remove samples
-def main():
-    ds_helper = DatasetHelper()
-    ds_helper.load(DATASET_FILE)
-
+# Basic example of adding/modifying/removing datasets
+def example1(ds_helper):
     # Add a sample by specifying all the relevant info directly
     ds_helper.updateDataset('new_dataset',
         dataset='/my_dataset/somwhere/on_DAS',
@@ -47,10 +44,49 @@ def main():
     # Remove a sample 
     ds.removeDataset('new_dataset')
 
+# Example of adding multiple new datasets via loops
+def example2(ds_helper):
+    ttZ_xsec = 0.244109
+    ttW_xsec = 0.341762
+    tZq_xsec = 0.07358
+    ttH_xsec = 0.5638
+
+    lst1 = [
+        ('tHq4fMatched',0.0),
+        ('tllq4fMatchedNoHiggs',tZq_xsec),
+        ('ttHJet',ttH_xsec),
+        ('ttlnuJet',ttW_xsec)
+    ]
+    lst2 = [
+        ('HanModel16DttllScanpoints_run1','EFTsp',True),
+        ('HanModelFixedStartingPt_run0','EFTfixed',True),
+        ('NoDim6_run0','SM',False)
+    ]
+
+    for proc,central_xsec in lst1:
+        for typ,suffix,is_eft in lst2:
+            hadoop_path = '/hadoop/store/user/awightma/postLHE_step/2019_04_19/ValidationHanModelPlusJet/v1/mAOD_step_%s_%s' % (proc,typ)
+            new_name = '%s_%s' % (proc,suffix)
+            ds_helper.updateDataset(new_name,
+                dataset='',
+                loc=hadoop_path,
+                is_eft=is_eft,
+                on_das=False,
+                central_xsec=central_xsec
+            )
+            print new_name
+            ds_helper.dump(new_name)
+
+def main():
+    ds_helper = DatasetHelper()
+    ds_helper.load(DATASET_FILE)
+
+    #example1(ds_helper)
+    #example2(ds_helper)
+
     # Save the dataset to json file
     if not args.dry_run:
         ds_helper.save(SAVE_FILE)
 
 if __name__ == "__main__":
-    #main()
-    pass
+    main()
