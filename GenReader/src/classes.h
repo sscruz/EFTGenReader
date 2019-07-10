14de4f3ec04c1528334d31ba67858b74ce07ef30
Andrew42 <awightma@nd.edu>
2019-07-10 12:53:13 -0400

    Add class def for TH1EFT

--- GenReader/src/classes.h
+++ GenReader/src/classes.h
@@ -1,5 +1,6 @@
 #include "EFTGenReader/GenReader/interface/WCPoint.h"
 #include "EFTGenReader/GenReader/interface/WCFit.h"
+#include "EFTGenReader/GenReader/interface/TH1EFT.h"
 
 #ifndef EFTGENREADER_DUMMY_OBJS
 #define EFTGENREADER_DUMMY_OBJS
@@ -8,6 +9,7 @@ namespace {
     struct WC_Objects {
         WCPoint dummy_wcpoint;
         WCFit dummy_wcfit;
+        TH1EFT dummy_th1eft;
     };
 }
 
