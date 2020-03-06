3925fb5a59eb8b1714542404c877604fd857700c
Kelci Mohrman <kmohrman@earth.crc.nd.edu>
2020-03-05 21:57:36 -0500

    Add qCut matching systematic

--- LHEReader/test/rwgt_validation/systWCdependenceCheck.C
+++ LHEReader/test/rwgt_validation/systWCdependenceCheck.C
@@ -36,6 +36,7 @@ void makePlot(TString sys, vector<WCPoint> pts_vect, vector<WCPoint> pts_vect_Wg
 
     std::string wc_name = "cpt"; // Should pass this to makePlot?
     TString save_name = sys+".png";
+    //TString save_name = "TEST.png";
     TString plot_name = "";
     TString x_axis_name = wc_name+" Strength";
     TString y_axis_name = "\\sigma_{NP}/\\sigma_{SM}";
@@ -132,8 +133,27 @@ void makePlot(TString sys, vector<WCPoint> pts_vect, vector<WCPoint> pts_vect_Wg
     //fit->SetMinimum(min_val);
     legend->Draw();
     c1->Print(save_name,"png");
+    //c1->Print("TEST.png","png");
     delete legend;
     delete c1;
+}
+
+std::map<string,string> make_wcpt_run_map() {
+    // Set up the wc point string (depends a lot on the naming scheme!):
+    // Right now this is set up for the 5 cpt axis scan files!!!
+    std::map<string,string> ref_pts_dict;
+    std::string wcname = "cpt";
+    float range_max = 15;
+    float npts = 5;
+    float step = (range_max*2)/(npts-1);
+    //std::cout << "STEP " << step << std::endl;
+    int run = 0;
+    for (float wcval=-range_max; wcval<=range_max; wcval=wcval+step){
+        ref_pts_dict["run"+std::to_string(run)] = "wcpt_"+wcname+"_"+std::to_string(wcval);
+        run = run + 1;
+        std::cout << "Run: " << run << " wc val: " << wcval << std::endl;
+    }
+    return ref_pts_dict;
 }
 
 // Returns a vector of 3 maps (for nominal, up, down), each map contains a vector of the 5 WCPoints for each systematic
@@ -157,6 +177,8 @@ std::vector<std::map<std::string,std::vector<WCPoint>>> get_WCpt_syst_maps(TStri
     int n_pts = all_dirs.size();
     std::cout << "Number of points to plot (number of files provied): " << n_pts << std::endl;
 
+    std::map<string,string> ref_pts_dict = make_wcpt_run_map(); // Very bad, depends very much on naming scheme!)
+    /*
     // Set up the wc point string (depends a lot on the naming scheme!):
     // Right now this is set up for the 5 cpt axis scan files!!!
     std::map<string,string> ref_pts_dict;
@@ -171,6 +193,7 @@ std::vector<std::map<std::string,std::vector<WCPoint>>> get_WCpt_syst_maps(TStri
         run = run + 1;
         std::cout << "Run: " << run << " wc val: " << wcval << std::endl;
     }
+    */
 
     // Loop over files
     for (int idx=0; idx<all_dirs.size(); idx++){
@@ -329,20 +352,172 @@ std::vector<std::map<std::string,std::vector<WCPoint>>> get_WCpt_syst_maps(TStri
             selection_pts_WgtD_map[sys].push_back(wcpt_systs_D_map[sys]);
         }
     }
-
     std::vector<std::map<std::string,std::vector<WCPoint>>> return_vect = {selection_pts_map,selection_pts_WgtU_map,selection_pts_WgtD_map};
     return return_vect;
-
-    //for(auto sys : sys_names){
-        //makePlot(sys,selection_pts_map["nominal"],selection_pts_WgtU_map[sys],selection_pts_WgtD_map[sys]);
-    //}
 }
 
+// Returns a vector of 3 maps (nominal, up, down), each map contains a vector of the 5 WCPoints for qCut sys
+std::vector<std::map<std::string,std::vector<WCPoint>>> get_WCpt_qCut_maps(TString run_dirs_file){
 
-void systWCdependenceCheck(TString run_dirs_file) {
+    std::map<std::string,std::vector<WCPoint>> selection_pts_map;
+    std::map<std::string,std::vector<WCPoint>> selection_pts_WgtU_map;
+    std::map<std::string,std::vector<WCPoint>> selection_pts_WgtD_map;
+    WCPoint sm_pt("smpt");
+
+    // Set up the wc point string (depends a lot on the naming scheme!):
+    std::map<string,string> ref_pts_dict = make_wcpt_run_map();
+
+    // Print dirs
+    std::vector<TString> all_dirs;
+    TString fdir;
+    std::ifstream input_filenames(run_dirs_file);
+    std::cout << "Using dirs:" << std::endl;
+    while (input_filenames >> fdir) {
+        all_dirs.push_back(fdir);
+        std::cout << "\t" << fdir << std::endl;
+    }
+    input_filenames.close();
+
+    // Loop over files
+    for (int idx=0; idx<all_dirs.size(); idx++){
+
+        fdir = all_dirs.at(idx);
+        std::string run_dir = getRunDirectory(fdir.Data());
+        std::cout << "[" << (idx+1) << "/" << all_dirs.size() << "] Full Path: " << fdir << std::endl;
+        std::vector<std::string> words;
+        split_string(run_dir,words,"_");
+        if (words.size() != 4) {
+            std::cout << "[WARNING] Skipping invalid run directory!" << std::endl;
+            continue;
+        }
+
+        // Get the tags from the file names
+        std::string process   = words.at(1);
+        std::string grp_tag   = words.at(2);
+        std::string run_label = words.at(3);
+        std::cout << "Process: " << process << " Grp tag: " << grp_tag << " Run label: " << run_label << std::endl;
+
+        // Check qCut type
+        std::string f_type = "";
+        if (grp_tag.find("qCut19") != std::string::npos){
+            f_type = "qCut_nom";
+            std::cout << "NOMINAL !!! " << grp_tag << std::endl;
+        } else if (grp_tag.find("qCut25") != std::string::npos){
+            f_type = "qCut_up";
+            std::cout << "up !!! " << grp_tag << std::endl;
+        } else if (grp_tag.find("qCut15") != std::string::npos){
+            f_type = "qCut_down";
+            std::cout << "down !!! " << grp_tag << std::endl;
+        }
+
+        // Chain together all root files in the run directory
+        TChain chain("EFTLHEReader/summaryTree");
+        auto dir_files = getFiles(fdir);
+        for (auto fn: dir_files) {
+            //std::cout << "\tFiles: " << fn << std::endl;
+            TString fname = fdir + "/" + fn;
+            chain.Add(fname);
+        }
+
+        // Set number of events in loop
+        int chain_entries = chain.GetEntries();
+        int last_entry = chain_entries;
+        if (chain_entries > 100000) { // 100k != 10min for some reason
+            std::cout << "Chain_entries: " << chain_entries << std::endl;
+            last_entry = 100000;
+        }
+        //last_entry = 10; // For testing
+        std::cout << "Last_entry: " << last_entry << std::endl;
+        int first_entry = 0;
+
+        WCFit* wcFit_intree = 0;
+        WCFit selection_fit;
+        WCFit selection_fit_WgtU;
+        WCFit selection_fit_WgtD;
+
+        double originalXWGTUP_intree = -1.;
+        double genLep_pt3_intree;
+        double genJet_pt4_intree;
+        chain.SetBranchAddress("originalXWGTUP",&originalXWGTUP_intree);
+        chain.SetBranchAddress("wcFit",&wcFit_intree);
+        chain.SetBranchAddress("genLep_pt3",&genLep_pt3_intree);
+        chain.SetBranchAddress("genJet_pt4",&genJet_pt4_intree);
+
+        // Set up the WC Points
+        //std::cout << "Run label: " << run_label << " , Dictionary entry: " << ref_pts_dict[run_label] << std::endl;
+        std::string pt_str = ref_pts_dict[run_label];
+        WCPoint selection_pt = WCPoint(pt_str); // Remember to only add wgt for events that pass selection
+        std::map<std::string,WCFit> wcfit_systs_U_map;
+        std::map<std::string,WCFit> wcfit_systs_D_map;
+        std::map<std::string,WCPoint> wcpt_systs_U_map;
+        std::map<std::string,WCPoint> wcpt_systs_D_map;
+        wcpt_systs_U_map["qCut"] = WCPoint(pt_str);
+        wcpt_systs_D_map["qCut"] = WCPoint(pt_str);
+
+        // Event loop:
+        int selection_events = 0;
+        for (int i = first_entry; i < last_entry; i++) {
+
+            chain.GetEntry(i);
+            if (genLep_pt3_intree > 10 and genJet_pt4_intree > 30){
+                selection_events = selection_events + 1;
+
+                // Nominal
+                //if (grp_tag.find("qCut19") != std::string::npos){
+                if (f_type == "qCut_nom"){
+                    selection_fit.addFit(*wcFit_intree);
+                    selection_pt.wgt += originalXWGTUP_intree;
+                }
+
+                // Scale the up fit and update WC pt
+                //if (grp_tag.find("qCut25") != std::string::npos){
+                if (f_type == "qCut_up"){
+                    wcfit_systs_U_map["qCut"].addFit(*wcFit_intree);
+                    wcpt_systs_U_map["qCut"].wgt += originalXWGTUP_intree;
+                }
+
+                // Scale the down fit and update WC pt
+                //if (grp_tag.find("qCut15") != std::string::npos){
+                if (f_type == "qCut_down"){
+                    wcfit_systs_D_map["qCut"].addFit(*wcFit_intree);
+                    wcpt_systs_D_map["qCut"].wgt += originalXWGTUP_intree;
+                }
+            }
+        }
+        std::cout << "\n Selected events over total: " << selection_events << "/" << last_entry << "->" << (float)selection_events/last_entry << "\n" << std::endl;
+        // Normalize to SM
+
+        if (f_type == "qCut_nom"){
+            double SM_xsec_sel = selection_fit.evalPoint(&sm_pt);
+            selection_pt.scale(1.0/SM_xsec_sel);
+            selection_pts_map["nominal"].push_back(selection_pt);
+        }
+
+        //selection_fit.scale(1.0/SM_xsec_sel);
+
+        if (f_type == "qCut_up"){
+            double SM_xsec_sel_WgtU = wcfit_systs_U_map["qCut"].evalPoint(&sm_pt);
+            wcpt_systs_U_map["qCut"].scale(1.0/SM_xsec_sel_WgtU);
+            //wcpt_systs_U_map["qCut"].scale(1.0/SM_xsec_sel); // Don't know how to do this norm since nominal was a different MG run?
+            selection_pts_WgtU_map["qCut"].push_back(wcpt_systs_U_map["qCut"]);
+        }
+        if (f_type == "qCut_down"){
+            double SM_xsec_sel_WgtD = wcfit_systs_D_map["qCut"].evalPoint(&sm_pt);
+            wcpt_systs_D_map["qCut"].scale(1.0/SM_xsec_sel_WgtD);
+            //wcpt_systs_D_map["qCut"].scale(1.0/SM_xsec_sel); // Don't know how to do this norm since nominal was a different MG run?
+            selection_pts_WgtD_map["qCut"].push_back(wcpt_systs_D_map["qCut"]);
+        }
+
+    }
+    std::vector<std::map<std::string,std::vector<WCPoint>>> return_vect = {selection_pts_map,selection_pts_WgtU_map,selection_pts_WgtD_map};
+    return return_vect;
+}
+
+void systWCdependenceCheck(TString run_dirs_file, TString run_dirs_qCuts_file) {
     gStyle->SetOptStat(0);
     std::vector<std::string> sys_names {"psISR","psFSR","muR","muF","muRmuF","nnpdf"};
 
+    ///*    
     std::vector<std::map<std::string,std::vector<WCPoint>>> systs_pts_vect;
     systs_pts_vect = get_WCpt_syst_maps(run_dirs_file);
     std::map<std::string,std::vector<WCPoint>> selection_pts_map      = systs_pts_vect.at(0);
@@ -352,4 +527,16 @@ void systWCdependenceCheck(TString run_dirs_file) {
     for(auto sys : sys_names){
         makePlot(sys,selection_pts_map["nominal"],selection_pts_WgtU_map[sys],selection_pts_WgtD_map[sys]);
     }
+    //*/
+    
+    ///*
+    std::cout << "\n ---------- Starting on the q cut calculations ----------\n" << std::endl;
+    std::vector<std::map<std::string,std::vector<WCPoint>>> qCut_pts_vect;
+    qCut_pts_vect = get_WCpt_qCut_maps(run_dirs_qCuts_file);
+    //std::map<std::string,std::vector<WCPoint>> selection_pts_map = qCut_pts_vect.at(0);
+    std::map<std::string,std::vector<WCPoint>> selection_pts_qCutU_map = qCut_pts_vect.at(1);
+    std::map<std::string,std::vector<WCPoint>> selection_pts_qCutD_map = qCut_pts_vect.at(2);
+    makePlot("qCut",selection_pts_map["nominal"],selection_pts_qCutU_map["qCut"],selection_pts_qCutD_map["qCut"]);
+    //*/
+
 }
